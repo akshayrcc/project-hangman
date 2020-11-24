650388cfbf3ba919df99ac48f80c2000ba5177fa
akshaychaudhari <akshaychaudhari12345@gmail.com>
2020-11-24 14:37:18 +0530

    @Before @After tested

--- src/test/TestHangman.java
+++ src/test/TestHangman.java
@@ -7,42 +7,52 @@ import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import main.Hangman;
 
 class TestHangman {
+	Random random;
+	Hangman hangman;
+	int requestedLength;
+
+	@BeforeAll
+	public void setupClass() {
+		random = new Random();
+		hangman = new Hangman();
+		hangman.loadWords();
+	}
+
+	@BeforeEach
+	public void setupTests() {
+		requestedLength = random.nextInt(6) + 5;
+	}
+
 	@Test
 	void test_alphabetCountInAWord() {
 		String word = "Pizza";
 		char alphabet = 'a';
-		Hangman hangman = new Hangman();
 		int count = hangman.countAlphabet(word, alphabet);
 		assertEquals(1, count);
 	}
 
 	@Test
 	void test_lengthOfFetchedWordRandom() {
-		Random ran = new Random();
-		int requestedLength = ran.nextInt(6) + 5;
 		System.out.println(" Random requestedLength is" + requestedLength);
-		Hangman hangman = new Hangman();
-		hangman.loadWords();
 		String word = hangman.fetchWord(requestedLength);
 		assertTrue(word.length() == requestedLength);
 	}
 
 	@Test
 	void uniquenessofFetchedWord() {
-		Random random = new Random();
-		int requestedLength = 0;
 		Set<String> usedWordSet = new HashSet<>();
 		int round = 0;
 		String word = null;
-		Hangman hangman = new Hangman();
-		hangman.loadWords();
 		while (round < 100) {
-			requestedLength = random.nextInt(6) + 5;
 			word = hangman.fetchWord(requestedLength);
 			round++;
 			assertTrue(usedWordSet.add(word));
@@ -51,14 +61,12 @@ class TestHangman {
 
 	@Test
 	void fetchClueBeforeAnyGuess() {
-		Hangman hangman = new Hangman();
 		String clue = hangman.fetchClue("Pizza");
 		assertEquals("-----", clue);
 	}
 
 	@Test
 	void fetchClueAfterACorrectGuess() {
-		Hangman hangman = new Hangman();
 		String clue = hangman.fetchClue("Pizza");
 		String newclue = hangman.fetchClue("Pizza", clue, 'a');
 		assertEquals("----a", newclue);
@@ -66,9 +74,19 @@ class TestHangman {
 
 	@Test
 	void fetchClueAfterAnIncorrectGuess() {
-		Hangman hangman = new Hangman();
 		String clue = hangman.fetchClue("Pizza");
 		String newclue = hangman.fetchClue("Pizza", clue, 'x');
 		assertEquals("-----", newclue);
+	}
+
+	@AfterEach
+	public void tearDownTest() {
+		// to close common functonality if used
+		requestedLength = 0;
+	}
+
+	@AfterAll
+	public void tearDownClass() {
+		// to close common functonality if used
 	}
 }
